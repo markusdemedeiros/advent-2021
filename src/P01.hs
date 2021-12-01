e87f86f21e5b0caa055af9ec91868fb81d18e55a
markusde <markusde@student.ubc.ca>
2021-12-01 09:01:29 -0800

    cleanup

--- src/P01.hs
+++ src/P01.hs
@@ -1,14 +1,17 @@
 module P01 where
 
-main :: IO ()
-main = do 
-    inp <- readFile "data/01/data.txt"
-    putStrLn . show . solve1 . map read . lines $ inp
-    putStrLn . show . solve2 . map read . lines $ inp
-
 
 solve1 :: [Int] -> Int
 solve1 i = length . filter (<0) $ zipWith (-) i (tail i)
 
 solve2 :: [Int] -> Int
 solve2 i = solve1 . zipWith (+) i $ zipWith (+) (tail i) (tail (tail i))
+
+parse :: IO [Int]
+parse = readFile "data/01/data.txt" >>= return . map read . lines
+
+solve :: IO ()
+solve = do
+    inp <- parse
+    parse >>= print . solve1
+    parse >>= print . solve2 
